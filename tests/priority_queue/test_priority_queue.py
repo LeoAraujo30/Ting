dcfe057cc4e8365bedc06a6146e23d26bf0a8af0
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-04-04 20:55:01 -0300

    requisitos 6 7 e 8 feitos

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,39 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    queue.enqueue({"qtd_linhas": 9})
+    queue.enqueue({"qtd_linhas": 4})
+    queue.enqueue({"qtd_linhas": 2})
+    queue.enqueue({"qtd_linhas": 5})
+    queue.enqueue({"qtd_linhas": 7})
+    queue.enqueue({"qtd_linhas": 11})
+    queue.enqueue({"qtd_linhas": 3})
+
+    assert queue.high_priority.data + queue.regular_priority.data == [
+        {"qtd_linhas": 4},
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 9},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 11}
+    ]
+
+    queue.dequeue()
+    queue.dequeue()
+
+    assert queue.high_priority.data + queue.regular_priority.data == [
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 9},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 11}
+    ]
+
+    assert queue.search(3) == {"qtd_linhas": 7}
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(10)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for dict in instance.data:
+        lines = []
+        for index, line in enumerate(dict["linhas_do_arquivo"]):
+            words = line.lower().replace(".", "").split(" ")
+            if word.lower() in words:
+                lines.append({"linha": index + 1})
+
+        if len(lines) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": dict["nome_do_arquivo"],
+                "ocorrencias": lines
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for dict in instance.data:
+        lines = []
+        for index, line in enumerate(dict["linhas_do_arquivo"]):
+            words = line.lower().replace(".", "").split(" ")
+            if word.lower() in words:
+                lines.append({"linha": index + 1, "conteudo": line})
+
+        if len(lines) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": dict["nome_do_arquivo"],
+                "ocorrencias": lines
+            })
+
+    return result
