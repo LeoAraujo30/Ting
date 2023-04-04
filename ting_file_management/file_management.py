9782ec648a38d54301effa652fe3edb86c4e6fe5
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-04-04 16:54:28 -0300

    requisitos 1 e 2 feitos

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua  implementação"""
+    try:
+        with open(path_file, mode="r") as file:
+            if path_file.endswith(".txt"):
+                return file.read().split("\n")
+
+            else:
+                sys.stderr.write("Formato inválido")
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value = self.data[0]
+        self.data = self.data[1:]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= len(self.data) - 1:
+            return self.data[index]
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
